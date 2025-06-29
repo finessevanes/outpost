e96a6b094ef4fdec750d34c59df97ed96896bbfd
Vanessa Mercado <vanessa.mercado24@gmail.com>
2025-06-29 20:08:47 +0200

    docs: things we  need

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -5,6 +7,32 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add Node.js polyfills required by AIR Kit
+    config.plugins.push(
+      new NodePolyfillPlugin({
+        additionalAliases: [
+          "buffer",
+          "crypto",
+          "assert",
+          "http",
+          "https",
+          "os",
+          "url",
+          "zlib",
+          "stream",
+          "_stream_duplex",
+          "_stream_passthrough",
+          "_stream_readable",
+          "_stream_writable",
+          "_stream_transform",
+          "process",
+        ],
+      })
+    );
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
